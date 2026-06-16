6f0217b3ff7ed53d5fbf49cdf2400be364958cbd
Jacques <f.lacerda@aluno.senai.br>
2026-06-16 08:37:23 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
 const app = require('../server');
-const serverless = require('serverless-http');
 
-// Exporta a função handler para o Vercel
-module.exports.handler = serverless(app);
+module.exports = app;
